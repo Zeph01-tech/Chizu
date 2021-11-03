9d47fc1a94458bb9fad1060b03a3789ddd5bc607
Kamaboko Gonpachiro <yashraj0492843@gmail.com>
2021-11-03 17:23:17 +0530

    Bhap

--- src/main/java/com/discord/chizu/Chizu.java
+++ src/main/java/com/discord/chizu/Chizu.java
@@ -3,16 +3,24 @@ package com.discord.chizu;
 import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import javax.security.auth.login.LoginException;
 
 class Context {
 	Guild guild;
 	User author;
+	List<Member> members;
 	MessageChannel channel;
 	long guild_id, author_id, channel_id;
 
@@ -20,9 +28,14 @@ class Context {
 		this.guild = event.getGuild();
 		this.guild_id = this.guild.getIdLong();
 		this.author = event.getAuthor();
+		this.members = event.getGuild().getMembers();
 		this.author_id = this.author.getIdLong();
 		this.channel = event.getChannel();
 		this.channel_id = this.channel.getIdLong();
+	}
+
+	public Member getMemberById(String id) {
+		return this.guild.getMemberById(id);
 	}
 
 	public void send(String message) {
@@ -73,19 +86,27 @@ class funcs {
 }
 
 public class Chizu extends ListenerAdapter {
-
 	public static String[] prefixes = {"Chizu", "chizu"};
 	private static JDA client;
-
-	public static void main(String[] args) throws LoginException {
+	private static EventWaiter waiter = new EventWaiter();
+	
+	public static void main(String[] args) throws LoginException, FileNotFoundException {
+		File file = new File("./TOKENS/token.txt");
+		Scanner content = new Scanner(file);
+		String token = content.nextLine();
+		content.close();
+		
 		client = JDABuilder
-					.createDefault("ODk2ODI2OTQwMzI4MTI4NTIz.YWMxLA.k6Zm4wsGXhJp6G0Oo9uDM0ewhfE")
+					.createDefault(token)
 					.setChunkingFilter(ChunkingFilter.ALL)
 					.enableIntents(
 						GatewayIntent.GUILD_MESSAGES,
-						GatewayIntent.GUILD_MEMBERS
+						GatewayIntent.GUILD_MEMBERS,
+						GatewayIntent.GUILD_MESSAGE_REACTIONS,
+						GatewayIntent.GUILD_EMOJIS
 					)
 					.addEventListeners(new Chizu())
+					.addEventListeners(waiter)
 					.setActivity(Activity.watching("L∅Ìf Ìj Ph∅N"))
 					.build();
 	}
@@ -93,37 +114,58 @@ public class Chizu extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(@NotNull MessageReceivedEvent event) {
 		String[] args = event.getMessage().getContentRaw().split(" ");
+
 		if (funcs.isValidPrefix(args[0])) {
 			if (args[1].equals("help"))
 				help(new Context(event));
 			else if (args[1].equals("?"))
 				answer(new Context(event));
 			else if (args[1].equals("addprefix") || args[1].equals("addp")) {
+				Context ctx = new Context(event);
 				if (args.length == 2)
-					if (event.getAuthor().getIdLong() == IDs.my_id)
-						event.getChannel().sendMessage("Specify a prefix to add").queue();
+					ctx.send("Specify a prefix to add");
 				else
-					addprefix(new Context(event), args[2]);
-			} else if (args[1].equals("removeprefix") || args[1].equals("removep"))
-				removeprefix(new Context(event), args[2]);
+					addprefix(ctx, args[2]);
+				
+			} else if (args[1].equals("removeprefix") || args[1].equals("removep")) {
+				Context ctx = new Context(event);
+				if (args.length == 2) 
+					ctx.send("Specify a prefix to remove");
+				else
+					removeprefix(ctx, args[2]);
 
-			  else if (args[1].equals("prefixlist") || args[1].equals("pl"))
+			} else if (args[1].equals("prefixlist") || args[1].equals("pl"))
 				prefix_list(new Context(event));
 
 			  else if (args[1].equals("greet")) {
+				Context ctx = new Context(event);
 				if (args.length == 2)
-					event.getChannel().sendMessage("Mention a user to greet").queue();
+					ctx.send("Mention a user to greet");
 				else {
 					String id = args[2].replaceAll("<@", " ").replace('!', ' ').replace('>', ' ').strip();
 					if (id.equals(args[2]))
-						event.getChannel().sendMessage("By all means, **I don't see any** `" + args[2] + "` **in our server** " + funcs.random_choice(new String[] {"._.", ".-.", "-_-"})).queue();
+						ctx.send("By all means, **I don't see any** `" + args[2] + "` **in our server** " + funcs.random_choice(new String[] {"._.", ".-.", "-_-"}));
 					else
-						greet(new Context(event), event.getGuild().getMemberById(id));
-					
+						greet(ctx, ctx.getMemberById(id));
 				}
-			} else if (args[1].equals("test")) {
-					System.out.println(args[2]);
-			}
+			} else if (args[1].equals("kick")) {
+				Context ctx = new Context(event);
+				if (ctx.author_id == IDs.my_id) {
+					if (args.length == 2)
+						ctx.send("Mention a user to kick");
+					else {
+						String id = args[2].replaceAll("<@", " ").replace("!", " ").replace(">", " ").strip();
+						if (id.equals(args[2]))
+							ctx.send("By all means, **I don't see any** `" + args[2] + "` **in our server** " + funcs.random_choice(new String[] {"._.", ".-.", "-_-"}));
+						else if (args.length == 3)
+							kick(ctx, ctx.getMemberById(id), "0");
+						else
+							kick(ctx, ctx.getMemberById(id), args[3]);
+					}
+				} else 
+					ctx.send("`kick` can only be used by the owner for now");
+			} else if (args[1].equals("test"))
+				test(new Context(event));
 		}
 	}
 
@@ -191,7 +233,32 @@ public class Chizu extends ListenerAdapter {
 	}
 
 	public void greet(Context ctx, Member member) {
-		String[] choices = {"Hi!! ", "Moshi Moshi!! ", "Hey! "};
 		ctx.send(funcs.random_choice(new String[] {"Hi!! ", "Moshi Moshi!! ", "Hey! "}) + member.getAsMention());
+	}
+
+	public void kick(Context ctx, Member member, String reason) {
+		try {
+			if (reason.equals("0"))
+				member.kick().queue();
+			else
+				member.kick(reason).queue();
+			
+			ctx.send("Successfully kicked " + member.getUser().getAsTag() + " from the server");
+		} catch (Exception e) {
+			ctx.send("Couldn't kick " + member.getAsMention() + " for some reason");
+		}
+	}
+
+	public void test(Context ctx) {
+		ctx.send("Send a message " + ctx.author.getAsMention());
+		waiter.waitForEvent(GuildMessageReceivedEvent.class, 
+		e -> e.getAuthor().getIdLong() == ctx.author_id && e.getChannel().getIdLong() == ctx.channel_id, 
+		(e) -> {
+			ctx.send("Yay even waiter is working!!");
+			waiter.waitForEvent(GuildMessageReceivedEvent.class, (f) -> f.getAuthor().getIdLong() == ctx.author_id && f.getChannel().getIdLong() == ctx.channel_id, 
+			(f) -> {
+				ctx.send("Bera gaand");
+			});
+		});
 	}
 }
